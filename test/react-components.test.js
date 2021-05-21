e6a085057e1e8d3784e888d2b134572f780c0a19
Colton Medlin <coltonmedlin@gmail.com>
2021-05-20 20:12:41 -0700

    update components test to be more flexible

--- test/react-components.test.js
+++ test/react-components.test.js
@@ -10,7 +10,7 @@ const testBook = {
   "title": "A Promised Land",
   "subtitle": "this is a subtitle",
   "author": "Barack Obama",
-  "narrator": "Barack Obama",
+  "narrator": "Michelle Obama",
   "imageUrl": "https://hr-rpt-audible.s3-us-west-1.amazonaws.com/001-a-promised-land.jpg",
   "audioSampleUrl": "https://hr-rpt-audible.s3-us-west-1.amazonaws.com/001-a-promised-land.mp3",
   "length": "29:10",
@@ -29,31 +29,20 @@ describe('COMPONENTS', () => {
 
   it('TITLE: should render the book title and subtitle', async () => {
     const component = await mount(<Title book={testBook}/>);
-    expect(component.containsAllMatchingElements([
-      <h1>A Promised Land</h1>,
-      <h2>this is a subtitle</h2>
-    ])).toBe(true);
+
+    expect(component.text()).toContain(testBook.title);
+    expect(component.text()).toContain(testBook.subtitle);
   });
 
   it('INFO: should render the book information', async () => {
     const component = await mount(<Info book={testBook}/>);
-    expect(component.containsAllMatchingElements([
-      <li>By: <a href="#">Barack Obama</a></li>,
-      <li>Narrated By: <a href="#">Barack Obama</a></li>,
-      <li>Length: 29 hrs and 10 mins</li>,
-      <li>Unabridged Audiobook</li>,
-      <li>Categories:
-        <span key='0'><a href="#">Biographies &amp; Memoirs</a>, </span>
-        <a key='1' href="#">Politics &amp; Activism</a>
-      </li>
-    ])).toBe(true);
-  });
 
-  it('IMAGE: should render the book image', async () => {
-    const component = await mount(<Image url={testBook.imageUrl}/>);
-    expect(component.containsAllMatchingElements([
-      <img height='100%' width='100%' src="https://hr-rpt-audible.s3-us-west-1.amazonaws.com/001-a-promised-land.jpg"></img>
-    ])).toBe(true);
+    expect(component.text()).toContain(testBook.author);
+    expect(component.text()).toContain(testBook.narrator);
+    expect(component.text()).toContain(testBook.version);
+    expect(component.text()).toContain('29 hrs and 10 mins');
+    expect(component.text()).toContain(testBook.categories[0].name);
+    expect(component.text()).toContain(testBook.categories[1].name);
   });
 
   it('IMAGE: should render the book image', async () => {
