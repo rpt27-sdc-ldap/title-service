e2df1c4c8969fd805d0769b8e1522772f16c95ae
Colton Medlin <coltonmedlin@gmail.com>
2021-05-29 12:46:10 -0700

    fix test issue

--- test/client-server-db.integration.test.js
+++ test/client-server-db.integration.test.js
@@ -5,7 +5,7 @@ describe('INTEGRATION: CLIENT-SERVER-DB', () => {
   it(`returns the proper book (DUNE) for the id 3`, async (done) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('http://localhost:2002/?id=3');
+      await page.goto('http://localhost:2002/?bookId=3');
       setTimeout(async () => {
         const title = await page.$eval('h1', e => e.innerHTML);
         expect(title).toBe('Dune');
@@ -17,7 +17,7 @@ describe('INTEGRATION: CLIENT-SERVER-DB', () => {
   it(`returns the proper book (Ready Player One) for the id 5`, async (done) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('http://localhost:2002/?id=5');
+      await page.goto('http://localhost:2002/?bookId=5');
       setTimeout(async () => {
         const title = await page.$eval('h1', e => e.innerHTML);
         expect(title).toBe('Ready Player One');
@@ -29,7 +29,7 @@ describe('INTEGRATION: CLIENT-SERVER-DB', () => {
   it(`returns and displays the proper book (The Martian) for the id 49`, async (done) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('http://localhost:2002/?id=49');
+      await page.goto('http://localhost:2002/?bookId=49');
       setTimeout(async () => {
         const title = await page.$eval('h1', e => e.innerHTML);
         expect(title).toBe('The Martian');
@@ -41,7 +41,7 @@ describe('INTEGRATION: CLIENT-SERVER-DB', () => {
   it(`returns and displays the proper book (Of Mice and Men) for the id 99`, async (done) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('http://localhost:2002/?id=99');
+      await page.goto('http://localhost:2002/?bookId=99');
       setTimeout(async () => {
         const title = await page.$eval('h1', e => e.innerHTML);
         expect(title).toBe('Of Mice and Men');
