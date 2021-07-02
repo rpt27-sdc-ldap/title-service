2891a87e3d024689fc4d9199dee1988b5355c05c
kage1414 <kylejohnson92294@gmail.com>
2021-07-02 00:37:19 -0400

    Add seed to postgres script

--- db/setup/seedPostgres.js
+++ db/setup/seedPostgres.js
@@ -1,5 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
+const seed = require('./modularDataGeneration.js').seed;
 
 const sequelize = new Sequelize('audible', process.env.PSQL_DB_USER, process.env.PSQL_DB_PASSWORD, {
   host: process.env.PSQL_DB_HOST,
