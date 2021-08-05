98eec4568354702f0eaf321f3be797d7b0abf95a
kage1414 <kylejohnson92294@gmail.com>
2021-08-05 11:50:58 -0400

    Add clustering

--- server/start.js
+++ server/start.js
@@ -2,6 +2,25 @@ require('newrelic');
 const app = require('./index.js');
 const port = 2002;
 
-app.listen(port, () => {
-  console.log(`Audible title service listening at http://localhost:${port}`);
-});
+const cluster = require('cluster');
+const cpus = require('os').cpus;
+const process = require('process');
+
+const numCPUs = cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Audible title service listening at http://localhost:${port}`);
+  });
+}
--- test/k6/k6.js
+++ test/k6/k6.js
@@ -4,7 +4,7 @@ export const options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 450,
+      rate: 10000,
       timeUnit: '1s',
       duration: '2m',
       preAllocatedVUs: 100,
